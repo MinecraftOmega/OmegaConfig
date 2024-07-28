15f5c9de46d61abade369794eb39cb5a6f859d1a
J-RAP <SrRapero720@hotmail.com>
2024-07-27 22:08:36 -0600

    we don't need the field type class

--- src/main/java/net/omegaloader/config/annotations/fields/ConfigField.java
+++ src/main/java/net/omegaloader/config/annotations/fields/ConfigField.java
@@ -1,9 +1,6 @@
 package net.omegaloader.config.annotations.fields;
 
-import net.omegaloader.config.builder.fields.BaseConfigField;
-
 public @interface ConfigField {
-    Class<?> value() default BaseConfigField.class;
     String name() default "";
 
     String i18n() default "";
--- src/test/java/me/srrrapero720/config/Sandbox.java
+++ src/test/java/me/srrrapero720/config/Sandbox.java
@@ -14,23 +14,36 @@ public class Sandbox {
     @Comment("This is a first-line comment")
     @Comment("Here i can add more details")
     @Comment("Conditions are attached on comments automatically")
-    @ConfigField(String.class)
-    @StringConditions(startsWith = "{", endsWith = "}", empty = false, value = "YEPA", mode = StringConditions.Mode.REGEX)
+    @StringConditions(startsWith = "{", endsWith = "}", empty = false)
+    @ConfigField
     public static String stringField = "{test_of_what_should_contain}";
 
+    @StringConditions(empty = false, value = "^[1-9a-z_]+:[1-9a-z_]+$", mode = StringConditions.Mode.REGEX)
+    @ConfigField
+    public static String resourceLocation = "examplemod:valid_resource_location";
+
     @Comment(value = {"Multiple comments", "but using just 1 annotation"})
-    @NumberConditions(minLong = -48, maxLong = 105)
-    @ConfigField(long.class)
+    @NumberConditions(minLong = -48, maxLong = 105, math = true, strictMath = true)
+    @ConfigField
     public static long longField = 0L;
 
-    @Comment("Single line comment")
-    @ConfigField(Yegoslovia.class)
+    @Comment(value = "Single line comment", i18n = "examplemod.config.simple_enum.with_i18n")
+    @ConfigField
     public static Yegoslovia simpleEnum = Yegoslovia.TINY;
 
     @Comment("Comment")
     @ArrayConditions(stringify = true, filter = CustomFilter.class)
-    @ConfigField(Yegoslovia.class)
+    @ConfigField
     public static Yegoslovia[] multiEnum = new Yegoslovia[] { Yegoslovia.GIANT, Yegoslovia.SMALL };
+
+    @Config(value = "sub_parent", i18n = "examplemod.config.parent.subparent.name")
+    public static class SubParent {
+
+        @StringConditions(empty = false, value = "^[1-9a-z_]+:[1-9a-z_]+$", mode = StringConditions.Mode.REGEX)
+        @ConfigField
+        public static String resourceLocation = "examplemod:valid_resource_location";
+
+    }
 
     // CAN WORKS WITH ANNOTATIONS OR VIA BUILDER
     @NumberConditions(minInt = -50, maxInt = 50)
